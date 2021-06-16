140b88b4475ea72c7d04d244254f21bc0180e68e
Ugo <okekeugochukwu.dev@gmail.com>
2021-06-17 00:59:53 +0100

    Control buttons added

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ for(fg = 0; fg < figures.length; fg++) {
 }
 
 
+var controlBtns = document.getElementsByTagName("button");
+
 
 
 //seconds
@@ -20,54 +22,58 @@ hour_Int2 = document.getElementById("hour-right_fig").innerText * 1;
 hour_Int1 = document.getElementById("hour-right_fig").innerText * 1;
 
 
-setInterval(() => {
-    seconds_Int2 ++;
-    document.getElementById("seconds-right_fig").innerText = seconds_Int2;
-
-    if(seconds_Int2 == 10) {
-        seconds_Int2 = 0;
-        document.getElementById("seconds-right_fig").innerText = 0;
-
-        seconds_Int1 = document.getElementById("seconds-left_fig").innerText;
-        seconds_Int1++;
-        document.getElementById("seconds-left_fig").innerText = seconds_Int1;
-    }
-
-    if(seconds_Int1 == 6) {
-        seconds_Int1 = 0
-        document.getElementById("seconds-left_fig").innerText = 0;
-
-        minute_Int2++;
-        document.getElementById("minute-right_fig").innerText = minute_Int2;
-    }
-    if (minute_Int2 == 10) {
-        minute_Int2 = 0;
-        document.getElementById("minute-right_fig").innerText = minute_Int2;
-
-        minute_Int1++;
-        document.getElementById("minute-left_fig").innerText = minute_Int1;
-
-    }
-    if (minute_Int1 == 6) {
-        minute_Int1 = 0;
-
-        hour_Int2++;
-        document.getElementById("hour-right_fig").innerText = hour_Int2;
-    }
-    if (hour_Int2 == 10){
-        hour_Int2 = 0;
-        document.getElementById("hour-right_fig").innerText = 0;
-
-        hour_Int1++;
-        document.getElementById("hour-left_fig").innerText = hour_Int1;
-    }
-    if (hour_Int1 == 2) {
-        hour_Int1, hour_Int2, minute_Int1, minute_Int2, seconds_Int1,seconds_Int2 = 0;
-    }
+function startTimer() {
+    setInterval(() => {
+        seconds_Int2 ++;
+        document.getElementById("seconds-right_fig").innerText = seconds_Int2;
     
+        if(seconds_Int2 == 10) {
+            seconds_Int2 = 0;
+            document.getElementById("seconds-right_fig").innerText = 0;
+    
+            seconds_Int1 = document.getElementById("seconds-left_fig").innerText;
+            seconds_Int1++;
+            document.getElementById("seconds-left_fig").innerText = seconds_Int1;
+        }
+        if(seconds_Int1 == 6) {
+            seconds_Int1 = 0
+            document.getElementById("seconds-left_fig").innerText = 0;
+    
+            minute_Int2++;
+            document.getElementById("minute-right_fig").innerText = minute_Int2;
+        }
 
-   
-}, 1000);
+        if (minute_Int2 == 10) {
+            minute_Int2 = 0;
+            document.getElementById("minute-right_fig").innerText = minute_Int2;
+    
+            minute_Int1++;
+            document.getElementById("minute-left_fig").innerText = minute_Int1;
+    
+        }
+        if (minute_Int1 == 6) {
+            minute_Int1 = 0;
+    
+            hour_Int2++;
+            document.getElementById("hour-right_fig").innerText = hour_Int2;
+        }
+
+        if (hour_Int2 == 10){
+            hour_Int2 = 0;
+            document.getElementById("hour-right_fig").innerText = 0;
+    
+            hour_Int1++;
+            document.getElementById("hour-left_fig").innerText = hour_Int1;
+        }
+        if (hour_Int1 == 2) {
+            hour_Int1, hour_Int2, minute_Int1, minute_Int2, seconds_Int1,seconds_Int2 = 0;
+        }
+        
+    
+       
+    }, 1000);
+    
+}
 
 function resetTimer() {
     figures = document.getElementsByClassName("fig"); var fg;
@@ -79,6 +85,12 @@ function resetTimer() {
     
 }
 
-document.body.addEventListener("click", function() {
-   resetTimer();
+controlBtns[0].addEventListener("click", function() {
+   startTimer();
+})
+controlBtns[1].addEventListener("click", function() {
+    stopTimer();
+})
+controlBtns[2].addEventListener("click", function() {
+    resetTimer();
 })
